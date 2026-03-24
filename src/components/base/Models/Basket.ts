f79d3c03b34f0c7ece279eb1a8cf0b2ff038622b
Kristina Guschina <kristyshka2001@yandex.com>
2026-03-24 15:35:00 +0300

    The project structure and documentation have been created

---
+++ src/components/base/Models/Basket.ts
@@ -0,0 +1,54 @@
+import { IProduct } from "../../../types";
+
+export class Basket {
+  private products: IProduct[] = [];
+
+  constructor() {}
+
+  //получениe массива товаров в корзине
+  getProducts(): IProduct[] {
+    return this.products;
+  }
+
+  /**
+   * добавлениe товара в корзину
+   * @param item - выбранный товар
+   */
+  addItem(item: IProduct): void {
+    this.products.push(item);
+  }
+
+  /**
+   * удалениe товара из корзины
+   * @param item - выбранный товар
+   */
+  deleteItem(item: IProduct): void {
+    const index = this.products.indexOf(item);
+    if (index > -1) {
+      this.products.splice(index, 1);
+    }
+  }
+
+  //очистка корзины
+  clearBascet(): void {
+    this.products = [];
+  }
+
+  //получениe стоимости всех товаров в корзине
+  getPriceProducts(): number {
+    return this.products.reduce((sum, item) => sum + (item.price ?? 0), 0);
+  }
+
+  // получениt количества товаров в корзине
+  getCount(): number {
+    return this.products.length;
+  }
+
+  /**
+   * проверка наличия товара в корзине по его id
+   * @param id - ID товара
+   */
+  hasItem(id: string): boolean {
+    return this.products.some((item) => item.id === id);
+  }
+}
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,42 @@
-export type ApiPostMethods = 'POST' | 'PUT' | 'DELETE';
+export type ApiPostMethods = "POST" | "PUT" | "DELETE";
+export type TPayment = "card" | "cash" | "";
 
 export interface IApi {
-    get<T extends object>(uri: string): Promise<T>;
-    post<T extends object>(uri: string, data: object, method?: ApiPostMethods): Promise<T>;
+  get<T extends object>(uri: string): Promise<T>;
+  post<T extends object>(
+    uri: string,
+    data: object,
+    method?: ApiPostMethods,
+  ): Promise<T>;
+}
+
+export interface IProduct {
+  id: string;
+  title: string;
+  image: string;
+  category: string;
+  price: number | null;
+  description: string;
+}
+
+export interface IBuyer {
+  payment: TPayment;
+  address: string;
+  email: string;
+  phone: string;
+}
+
+export interface IOrder {
+  buyer: IBuyer;
+  item: IProduct;
+}
+
+export interface IResponseOrder {
+  id: string;
+  total: number;
+}
+
+export interface IOrderProducts {
+  items: [];
+  total: number;
 }
